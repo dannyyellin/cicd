f4e7f4fc50da5aeb55b72cb6aa57724b14f8390d
Danny Yellin <34856327+dannyyellin@users.noreply.github.com>
2025-01-08 10:09:17 +0200

    updated test-toys.py

--- pytest-code/test-toys.py
+++ pytest-code/test-toys.py
@@ -23,31 +23,23 @@ urlToys = 'http://localhost:8001/toys'
 
 def assert_fields_equal(record1: dict, record2: any):
     if type(record2) != type(record1):  # check that record2 is also a dictionary
-        # print(f'assert_fields_equal: Returned object is not of type {str(type(record1))}\n')
         return False
     for field in record1.keys():
         if field not in record2:
-            # print(f'assert_fields_equal: Expected field named "{field}" in returned object but it was missing. Returned object = {record2}\n')x
             return False
         if record1[field] != record2[field]:
-            # print(f'assert_fields_equal: Field "{field}" in returned object: {record2[field]} does not match expected value: {record1[field]}')
             return False
-    # print("passed assert_fields_equal")
     return True
 
 
 # test_collection_contains_field_values checks that given a list of objects, a json field name field, and a list of
 # values, for each v in the list of values there exists a record r in coll such that r[field] = v
 def assert_collection_contains_field_values(coll: list, field: str, values: list):
-    # print(f"assert_collection_container_field_values:  coll = {coll}\n field = {field}\n values = {values}")
     for v in values:
         try:
             if not [c for c in coll if c[field] == v]:
-                # print(f"assert_collection_contains_field_values: No object in returned array with {field} == {v}")
                 return False
         except Exception as e:
-            # print("failed assert_collection_contains_field_values")
-            # print("Exception = ", str(e))
             return False
     # otherwise found that the collection satisfies the assertion
     return True
@@ -59,7 +51,6 @@ def test_post_toy1():
     response = requests.post(urlToys,
         headers={"Content-Type":"application/json"},
         data=json_data)
-    # print("response from POST toy1 is ", response.json())
     id1 = response.json()['id']
     assert response.status_code == 201
 
@@ -67,7 +58,6 @@ def test_post_toy1():
 def test_get_toy1():
     global id1
     response2 = requests.get(urlToys + '/' + id1)
-    # print(f"response from GET toys {id1} is {response2.json()}")
     assert response2.status_code == 200
     assert_fields_equal(toy1, response2.json())
 
@@ -78,7 +68,6 @@ def test_post_toy2():
     response3 = requests.post(urlToys,
         headers={"Content-Type":"application/json"},
         data=json_data)
-    # print(f"response from POST toy2 is {response3.json()}")
     id2 = response3.json()['id']
     assert response3.status_code == 201
 
@@ -86,7 +75,6 @@ def test_post_toy2():
 def test_get_toy2():
     global id2, allToys
     response2 = requests.get(urlToys + '/' + id2)
-    # print(f"response from GET toys {id2} is {response2.json()}")
     assert response2.status_code == 200
     assert_fields_equal(toy1, response2.json())
 
@@ -98,4 +86,6 @@ def test_get_all():
     # assert that the collection returned contains toys with id1 and id2
     assert_collection_contains_field_values(all_toys, "id", [id1, id2])
 
-# to see print statements, issue command: pytest -s
+# cmd:  pytest -vv -s test-toys.py
+# -vv is very verbose
+# -s is to show results of print stmts to console (I removed the print statements from this version)
